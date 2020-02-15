0388277477d9350d3cbeb35539fdeb36b8922fd8
MAXIMNSK <Maximillian69@rambler.ru>
2020-02-15 18:17:30 +0700

    Личный проект: больше деталей (часть 1) - исправил замечания - 2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 'use strict';
 
 var COUNT_OBJECTS = 25;
-
-var arrayObjects = [];
 var LIKES_MIN = 15;
 var LIKES_MAX = 200;
 var ARRAY_MESSAGE = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var ARRAY_NAMES = ['Артём', 'Вадим', 'Пётр', 'Ксения', 'Анна', 'Мария', 'Дмитрий', 'Игорь', 'Михаил', 'Ольга', 'Кристина', 'Елена'];
+var arrayObjects = [];
 var shadowBlock = document.createDocumentFragment();
 var templatePicture = document.querySelector('#picture').content.querySelector('.picture');
 var targetContentBlock = document.querySelector('.pictures');
 
 for (var i = 0; i < COUNT_OBJECTS; i++) {
-  arrayObjects.push(createElementPhoto());
-
-  for (var x = 0; x < getRandomNumber(1, 5); x++) {
-    arrayObjects[i].comments.push(fillCommentsArray());
-  }
+  arrayObjects.push(createElementPhoto(i));
 }
 
 /**
@@ -31,14 +26,15 @@ function getRandomNumber(minValue, maxValue) {
 
 /**
  * Функция возвращает объект-фото
+ * @param {number} iteration номер итерации передаваемый в объет, для выбора номера картинки в свойстве url
  * @return {Object} возвращает объект со следующими свойствами: адрес фото / описание / количество лайков / список комментариев к фото
  */
-function createElementPhoto() {
+function createElementPhoto(iteration) {
   return {
-    url: 'photos/' + (i + 1) + '.jpg',
+    url: 'photos/' + (iteration + 1) + '.jpg',
     description: 'description photo',
     likes: getRandomNumber(LIKES_MIN, LIKES_MAX),
-    comments: [fillCommentsArray()]
+    comments: fillCommentsArray() // заполнить этот блядский массив  // вызов функции с сгенерированным числом от 1 до 5, а в целевой функции создать чистый массив и цикл через который будет добавляться объект в чистый массив, а когда условие будет выполнено (счетчик равен передаваемому числу - то в return пихнуть тот массив)
   };
 }
 
@@ -47,11 +43,18 @@ function createElementPhoto() {
  * @return {Object} возвращает комментарий к фото, со следующими свойствами: аватар пользователя / сообщение / имя
  */
 function fillCommentsArray() {
-  return {
-    avatar: 'img/avatar-' + getRandomNumber(1, 7) + '.svg',
-    message: ARRAY_MESSAGE[getRandomNumber(1, ARRAY_MESSAGE.length)],
-    name: ARRAY_NAMES[getRandomNumber(1, ARRAY_NAMES.length)]
-  };
+  var random = getRandomNumber(1, 5);
+  var tempArray = [];
+
+  while (random > 0) {
+    tempArray.push({
+      avatar: 'img/avatar-' + getRandomNumber(1, 7) + '.svg',
+      message: ARRAY_MESSAGE[getRandomNumber(1, ARRAY_MESSAGE.length)],
+      name: ARRAY_NAMES[getRandomNumber(1, ARRAY_NAMES.length)]
+    });
+    random--;
+  }
+  return tempArray;
 }
 
 /**
