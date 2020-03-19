493f62af63d0bc0a3400efd7b885ce704b471491
MAXIMNSK <Maximillian69@rambler.ru>
2020-03-19 21:07:50 +0700

    Личный проект: надо подкачаться (часть 2). Исправления 5

--- js/network.js
+++ js/network.js
@@ -27,9 +27,63 @@
     window.collectionPhoto.render(responseServer);
   }
 
-  function responseError() {
-    // error code
+  function responseError(code) {
+    switch (code) {
+      case code >= 300:
+        errorMessage(code);
+        break;
+
+      default:
+        return;
+    }
+  }
+
+  function errorMessage(code) {
+    var newDiv = document.createElement('div');
+    newDiv.style.position = 'absolute';
+    newDiv.style.width = '100%';
+    newDiv.style.backgroundColor = 'red';
+    newDiv.textContent = 'Ошибка при загрузке! Код: ' + code;
+    newDiv.style.textAlign = 'center';
+    newDiv.style.color = 'white';
+    window.utility.body.prepend(newDiv);
+
+    document.addEventListener('click', function () {
+      newDiv.remove();
+    });
+
+    document.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === window.utility.keyEsc) {
+        newDiv.remove();
+      }
+    });
   }
 
   download(responseOk, responseError);
+})();
+
+// отправка данных на сервер
+(function () {
+  var URL = 'https://js.dump.academy/kekstagram';
+  var TIMEOUT = 10000;
+
+  function upload(data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', function () {
+      onLoad(xhr.response);
+    });
+
+    xhr.addEventListener('error', function () {
+      onError(xhr.status);
+    });
+
+    xhr.timeout = TIMEOUT;
+    xhr.open('POST', URL);
+    xhr.send(data);
+  }
+
+  window.network = {
+    upload: upload,
+  };
 })();
